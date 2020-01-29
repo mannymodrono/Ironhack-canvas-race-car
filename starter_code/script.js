45ea3a76613f659ecf332d4b8dadd6e4ea232027
Manny Modrono <21243@columbushs.com>
2020-01-29 09:08:35 -0500

    made static image

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,31 @@
 window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+
+    context.fillStyle='green';
+    context.fillRect(0, 0, 600, 925);
+    context.fillStyle = 'grey';
+    context.fillRect(30, 0, 540, 925)
+    context.fillStyle='white';
+    context.fillRect(40, 0, 15, 925);
+    context.fillRect(540, 0, 15, 925);
+    context.fillRect(300, 0, 15, 80);
+    context.fillRect(300, 100, 15, 80);
+    context.fillRect(300, 200, 15, 80);
+    context.fillRect(300, 300, 15, 80);
+    context.fillRect(300, 400, 15, 80);
+    context.fillRect(300, 500, 15, 80);
+    context.fillRect(300, 600, 15, 80);
+    context.fillRect(300, 700, 15, 80);
+    context.fillRect(300, 800, 15, 80);
+    context.fillRect(300, 900, 15, 80);
+    context.fillRect(300, 1000, 15, 80);
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-
+    
   }
 };
