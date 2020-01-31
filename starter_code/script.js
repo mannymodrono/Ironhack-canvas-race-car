042dbf8b73e14e7d4efb10413c1efe80d37bb15f
Manny Modrono <21243@columbushs.com>
2020-01-31 11:14:42 -0500

    added moving road

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,31 +1,56 @@
 window.onload = function() {
   var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
+  var context = canvas.getContext('2d');
 
-    context.fillStyle='green';
-    context.fillRect(0, 0, 600, 925);
-    context.fillStyle = 'grey';
-    context.fillRect(30, 0, 540, 925)
-    context.fillStyle='white';
-    context.fillRect(40, 0, 15, 925);
-    context.fillRect(540, 0, 15, 925);
-    context.fillRect(300, 0, 15, 80);
-    context.fillRect(300, 100, 15, 80);
-    context.fillRect(300, 200, 15, 80);
-    context.fillRect(300, 300, 15, 80);
-    context.fillRect(300, 400, 15, 80);
-    context.fillRect(300, 500, 15, 80);
-    context.fillRect(300, 600, 15, 80);
-    context.fillRect(300, 700, 15, 80);
-    context.fillRect(300, 800, 15, 80);
-    context.fillRect(300, 900, 15, 80);
-    context.fillRect(300, 1000, 15, 80);
+  var img = new Image();
+  img.src = 'images/road.PNG';
+  let carimg = new Image();
+  carimg.src = 'images/car.png'
 
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-    
+
   }
+
+var backgroundImage = {
+  img: img,
+  x: 0,
+  speed: -1,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= canvas.width;
+  },
+
+  draw: function() {
+    context.drawImage(this.img, this.x, 0);
+    if (this.speed < 0) {
+      context.drawImage(this.img, this.x + canvas.width, 0);
+    } else {
+      context.drawImage(this.img, this.x - this.img.width, 0);
+    }
+  },
+};
+
+let carImg = {
+  img: carimg,
+  y: 0,
+
+  move: function
+}
+
+function updateCanvas() {
+  backgroundImage.move();
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundImage.draw();
+
+  requestAnimationFrame(updateCanvas);
+}
+
+// start calling updateCanvas once the image is loaded
+img.onload = updateCanvas;
 };
